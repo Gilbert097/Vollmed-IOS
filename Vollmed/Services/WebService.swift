cd4e80fb0e846d6cdac84e537aa175a52e5802bc
Gilberto Silva <betosilva.luz@gmail.com>
2024-10-04 19:04:04 -0300

    Implementando requisição para recuperar especialistas.

--- Vollmed/Services/WebService.swift
+++ Vollmed/Services/WebService.swift
@@ -11,7 +11,18 @@ struct WebService {
     
     private let baseURL = "http://localhost:3000"
     
-    func getAllSpecialists() {
+    func getAllSpecialists() async throws -> [Specialist]? {
+        let path = baseURL + "/especialista"
         
+        guard let url = URL(string: path) else {
+            print("Erro na requisição.")
+            return nil
+        }
+        
+        let request = URLRequest(url: url)
+        let (data, _) = try await URLSession.shared.data(for: request)
+        let specialists = try JSONDecoder().decode([Specialist].self, from: data)
+        
+        return specialists
     }
 }
