a787017fde51305c014b4255f1655ae1543ba6b5
Gilberto Silva <betosilva.luz@gmail.com>
2024-10-07 15:39:54 -0300

    Implementando cache de imagens.

--- Vollmed/Services/WebService.swift
+++ Vollmed/Services/WebService.swift
@@ -10,18 +10,29 @@ import UIKit
 struct WebService {
     
     private let baseURL = "http://localhost:3000"
+    private let imageCache = NSCache<NSString, UIImage>()
     
-    func downloadImage(from urlImage: String) async throws -> UIImage? {
+    func downloadImage(from imageURL: String) async throws -> UIImage? {
         
-        guard let url = URL(string: urlImage) else {
+        guard let url = URL(string: imageURL) else {
             print("Erro na requisição.")
             return nil
         }
         
+        if let cachedImage = self.imageCache.object(forKey: imageURL as NSString) {
+            return cachedImage
+        }
+        
         let request = URLRequest(url: url)
         let (data, _) = try await URLSession.shared.data(for: request)
-       
-        return UIImage(data: data)
+        
+        guard let image = UIImage(data: data) else {
+            return nil
+        }
+        
+        self.imageCache.setObject(image, forKey: imageURL as NSString)
+        
+        return image
     }
     
     func getAllSpecialists() async throws -> [Specialist]? {
