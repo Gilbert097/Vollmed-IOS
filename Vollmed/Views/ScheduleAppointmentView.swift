d6a0cc4cdd4f9d95c529079a3fa7aa466bc5ff79
Gilberto Silva <betosilva.luz@gmail.com>
2024-10-07 19:56:05 -0300

    Exibindo alerta ao tentar agendar consulta.

--- Vollmed/Views/ScheduleAppointmentView.swift
+++ Vollmed/Views/ScheduleAppointmentView.swift
@@ -12,6 +12,9 @@ let patientID = "46b84859-8d2c-4dda-bde7-a2018c5f55d7"
 struct ScheduleAppointmentView: View {
     
     @State private var selectedDate = Date()
+    @State private var isAppointmentScheduled = false
+    @State private var showAlert = false
+    
     let specialistID: String
     private let service = WebService()
     
@@ -25,10 +28,15 @@ struct ScheduleAppointmentView: View {
             if let response = try await service.sheduleAppointment(appointmentResquest: request) {
                 print("Consulta agendada com sucesso!")
                 print(response)
+                isAppointmentScheduled = true
+            } else {
+                isAppointmentScheduled = false
             }
         } catch {
+            isAppointmentScheduled = false
             print("Ocorreu um erro ao realizar agendamento: \(error)")
         }
+        showAlert = true
     }
     
     var body: some View {
@@ -56,6 +64,17 @@ struct ScheduleAppointmentView: View {
         .navigationBarTitleDisplayMode(.large)
         .onAppear {
             UIDatePicker.appearance().minuteInterval = 15
+        }
+        .alert( isAppointmentScheduled ? "Sucesso!" : "Ops, algo deu errado!", isPresented: $showAlert, presenting: isAppointmentScheduled) { _ in
+            Button(action: {}, label: {
+                Text("Ok")
+            })
+        } message: { isScheduled in
+            if isScheduled {
+                Text("A consulta foi agendada com sucesso!")
+            } else {
+                Text("Houve um erro ao agendar sua consulta. Por favor tente novamente ou entre em contato por telefone.")
+            }
         }
     }
 }
