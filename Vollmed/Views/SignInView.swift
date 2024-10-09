16b2ab73d625ab3c2589c190ecad077d4f29ab95
Gilberto Silva <betosilva.luz@gmail.com>
2024-10-09 13:41:04 -0300

    Criando tela de login.

---
+++ Vollmed/Views/SignInView.swift
@@ -0,0 +1,61 @@
+//
+//  SignInView.swift
+//  Vollmed
+//
+//  Created by Gilberto Silva on 09/10/24.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    
+    @State private var email: String = .init()
+    @State private var password: String = .init()
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            
+            Image(.logo)
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity, maxHeight: 36, alignment: .center)
+            
+            Text("Olá!")
+                .font(.title2)
+                .bold()
+                .foregroundStyle(.accent)
+            
+            Text("Preencha para acessar sua conta.")
+                .font(.title3)
+                .bold()
+                .foregroundStyle(.gray)
+                .padding(.bottom)
+            
+            Text("Email")
+                .font(.title3)
+                .bold()
+                .foregroundStyle(.accent)
+            
+            TextField("Insira seu email", text: $email)
+                .padding(14)
+                .background(.gray.opacity(0.25))
+                .clipShape(RoundedRectangle(cornerRadius: 14))
+                .keyboardType(.emailAddress)
+                .textInputAutocapitalization(.never)
+            
+            Text("Senha")
+                .font(.title3)
+                .bold()
+                .foregroundStyle(.accent)
+            
+            SecureField("Insira sua senha", text: $password)
+                .padding(14)
+                .background(.gray.opacity(0.25))
+                .clipShape(RoundedRectangle(cornerRadius: 14))
+        }
+    }
+}
+
+#Preview {
+    SignInView()
+}
