b39280d9175fa4548709e38fd212b05128728cad
Gilberto Silva <betosilva.luz@gmail.com>
2024-10-10 11:18:39 -0300

    Finalizando tela de cadastro.

--- Vollmed/Views/SignUpView.swift
+++ Vollmed/Views/SignUpView.swift
@@ -8,9 +8,18 @@
 import SwiftUI
 
 struct SignUpView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    @State private var name: String = .init()
+    @State private var email: String = .init()
+    @State private var cpf: String = .init()
+    @State private var telephone: String = .init()
+    @State private var password: String = .init()
+    
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 16) {
+                
                 Image(.logo)
                     .resizable()
                     .scaledToFit()
@@ -27,7 +36,75 @@ struct SignUpView: View {
                     .bold()
                     .foregroundStyle(.gray)
                     .padding(.bottom)
+                
+                Text("Nome")
+                    .font(.title3)
+                    .bold()
+                    .foregroundStyle(.accent)
+                
+                TextField("Insira seu nome", text: $name)
+                    .padding(14)
+                    .background(.gray.opacity(0.25))
+                    .clipShape(RoundedRectangle(cornerRadius: 14))
+                
+                Text("Email")
+                    .font(.title3)
+                    .bold()
+                    .foregroundStyle(.accent)
+                
+                TextField("Insira seu email", text: $email)
+                    .padding(14)
+                    .background(.gray.opacity(0.25))
+                    .clipShape(RoundedRectangle(cornerRadius: 14))
+                    .keyboardType(.emailAddress)
+                    .textInputAutocapitalization(.never)
+                
+                Text("CPF")
+                    .font(.title3)
+                    .bold()
+                    .foregroundStyle(.accent)
+                
+                TextField("Insira seu CPF", text: $cpf)
+                    .padding(14)
+                    .background(.gray.opacity(0.25))
+                    .clipShape(RoundedRectangle(cornerRadius: 14))
+                    .keyboardType(.numberPad)
+                
+                Text("Telefone")
+                    .font(.title3)
+                    .bold()
+                    .foregroundStyle(.accent)
+                
+                TextField("Insira seu telefone", text: $telephone)
+                    .padding(14)
+                    .background(.gray.opacity(0.25))
+                    .clipShape(RoundedRectangle(cornerRadius: 14))
+                    .keyboardType(.numberPad)
+                
+                Text("Senha")
+                    .font(.title3)
+                    .bold()
+                    .foregroundStyle(.accent)
+                
+                SecureField("Insira sua senha", text: $password)
+                    .padding(14)
+                    .background(.gray.opacity(0.25))
+                    .clipShape(RoundedRectangle(cornerRadius: 14))
+                
+                Button {
                     
+                } label: {
+                    ButtonView(text: "Cadastrar")
+                }
+                
+                Button {
+                    presentationMode.wrappedValue.dismiss()
+                } label: {
+                    Text("Já possui uma conta? faça o login!")
+                        .bold()
+                        .frame(maxWidth: .infinity, alignment: .center)
+                }
+                
             }
         }
         .scrollIndicators(.never)
