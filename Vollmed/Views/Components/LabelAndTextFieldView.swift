490b65e7cb960fd95441ca7cfd43fc147a3cc396
Gilberto Silva <betosilva.luz@gmail.com>
2024-10-10 16:31:14 -0300

    Tratando entrada de senha no componente LabelAndTextFieldView.

--- Vollmed/Views/Components/LabelAndTextFieldView.swift
+++ Vollmed/Views/Components/LabelAndTextFieldView.swift
@@ -8,39 +8,50 @@
 import SwiftUI
 
 struct LabelAndTextFieldView: View {
-   
+    
     @Binding var text: String
     
     let label: String
     let placeHolder: String
     let type: UIKeyboardType
     let autocapitalization: TextInputAutocapitalization?
+    let isSecureField: Bool
     
     public init(text: Binding<String>,
                 label: String,
                 placeHolder: String,
                 type: UIKeyboardType = .default,
-                autocapitalization: TextInputAutocapitalization? = nil) {
+                autocapitalization: TextInputAutocapitalization? = nil,
+                isSecureField: Bool = false) {
         self._text = text
         self.label = label
         self.placeHolder = placeHolder
         self.type = type
         self.autocapitalization = autocapitalization
+        self.isSecureField = isSecureField
     }
     
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
+            
             Text(label)
                 .font(.title3)
                 .bold()
                 .foregroundStyle(.accent)
             
-            TextField(placeHolder, text: $text)
-                .padding(14)
-                .background(.gray.opacity(0.25))
-                .clipShape(RoundedRectangle(cornerRadius: 14))
-                .keyboardType(type)
-                .textInputAutocapitalization(autocapitalization)
+            if isSecureField {
+                SecureField(placeHolder, text: $text)
+                    .padding(14)
+                    .background(.gray.opacity(0.25))
+                    .clipShape(RoundedRectangle(cornerRadius: 14))
+            } else {
+                TextField(placeHolder, text: $text)
+                    .padding(14)
+                    .background(.gray.opacity(0.25))
+                    .clipShape(RoundedRectangle(cornerRadius: 14))
+                    .keyboardType(type)
+                    .textInputAutocapitalization(autocapitalization)
+            }
         }
     }
 }
