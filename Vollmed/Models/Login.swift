eea21e17180b87d4c9dd87807fef71f15b050896
Gilberto Silva <betosilva.luz@gmail.com>
2024-10-11 10:33:14 -0300

    Criando modelo de login.

---
+++ Vollmed/Models/Login.swift
@@ -0,0 +1,25 @@
+//
+//  Login.swift
+//  Vollmed
+//
+//  Created by Gilberto Silva on 11/10/24.
+//
+
+import Foundation
+
+
+struct LoginRequest: Codable {
+    let email: String
+    let password: String
+    
+    enum CodingKeys: String, CodingKey {
+        case email
+        case password = "senha"
+    }
+}
+
+struct LoginResponse: Identifiable, Codable {
+    let id: String
+    let auth: Bool
+    let token: String
+}
