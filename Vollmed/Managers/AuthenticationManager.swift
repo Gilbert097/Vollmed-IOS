75470588b66698ce5906abe44875b6febd216a31
Gilberto Silva <betosilva.luz@gmail.com>
2024-10-15 14:44:04 -0300

    Criando AuthenticationManager.

---
+++ Vollmed/Managers/AuthenticationManager.swift
@@ -0,0 +1,41 @@
+//
+//  AuthenticationManager.swift
+//  Vollmed
+//
+//  Created by Gilberto Silva on 15/10/24.
+//
+
+import Foundation
+
+class AuthenticationManager {
+    
+    var token: String?
+    var patientID: String?
+    private let tokenKey = "app-vollmed-token"
+    private let patientIDKey = "app-vollmed-patient-id"
+    
+    init() {
+        self.token = KeychainHelper.get(for: self.tokenKey)
+        self.patientID = KeychainHelper.get(for: self.patientIDKey)
+    }
+    
+    func saveToken(token: String) {
+        KeychainHelper.save(value: token, key: self.tokenKey)
+        self.token = token
+    }
+    
+    func removeToken() {
+        KeychainHelper.remove(for: self.tokenKey)
+        self.token = nil
+    }
+    
+    func savePatientID(id: String) {
+        KeychainHelper.save(value: id, key: self.patientIDKey)
+        self.patientID = id
+    }
+    
+    func removePaitentID() {
+        KeychainHelper.remove(for: self.patientIDKey)
+        self.patientID = nil
+    }
+}
