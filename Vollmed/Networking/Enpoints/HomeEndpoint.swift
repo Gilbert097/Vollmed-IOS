1038fa3ccd4d0d5b7d448e4c3f5d6ea86fae3030
Gilberto Silva <betosilva.luz@gmail.com>
2024-10-29 18:39:16 -0300

    Implementando HomeEndpoint.

---
+++ Vollmed/Networking/Enpoints/HomeEndpoint.swift
@@ -0,0 +1,44 @@
+//
+//  HomeEndpoint.swift
+//  Vollmed
+//
+//  Created by Gilberto Silva on 29/10/24.
+//
+
+import Foundation
+
+enum HomeEndpoint {
+    case getAllSpecialists
+}
+
+extension HomeEndpoint: Enpoint {
+    var path: String {
+        switch self {
+        case .getAllSpecialists:
+            return "/especialista"
+        }
+    }
+    
+    var method: RequestMethod {
+        switch self {
+        case .getAllSpecialists:
+            return .get
+        }
+    }
+    
+    var header: [String : String]? {
+        switch self {
+        case .getAllSpecialists:
+            return nil
+        }
+    }
+    
+    var body: [String : String]? {
+        switch self {
+        case .getAllSpecialists:
+            return nil
+        }
+    }
+    
+    
+}
