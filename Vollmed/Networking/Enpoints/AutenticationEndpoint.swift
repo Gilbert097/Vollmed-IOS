8223c707fe1da6fe33cc6c99ef8c64d0da13ae7f
Gilberto Silva <betosilva.luz@gmail.com>
2024-11-04 16:14:37 -0300

    Implementação dos endpoints de autentificação.

---
+++ Vollmed/Networking/Enpoints/AutenticationEndpoint.swift
@@ -0,0 +1,44 @@
+//
+//  AutenticationEndpoint.swift
+//  Vollmed
+//
+//  Created by Gilberto Silva on 04/11/24.
+//
+
+import Foundation
+
+enum AutenticationEndpoint {
+    case logout
+}
+
+extension AutenticationEndpoint: Enpoint {
+    var path: String {
+        switch self {
+        case .logout:
+            return "/auth/logout"
+        }
+    }
+    
+    var method: RequestMethod {
+        switch self {
+        case .logout:
+            return .post
+        }
+    }
+    
+    var header: [String : String]? {
+        switch self {
+        case .logout:
+            guard  let token = AuthenticationManager.shared.token else { return nil }
+            
+            return ["Authorization": "Bearer \(token)"]
+        }
+    }
+    
+    var body: [String : String]? {
+        switch self {
+        case .logout:
+            return nil
+        }
+    }
+}
